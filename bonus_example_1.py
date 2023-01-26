0cefea0d6fa922ad192443f07619efea2bcda77e
mpwagner1 <94921116+mpwagner1@users.noreply.github.com>
2023-01-26 14:36:31 -0600

    Add files via upload

---
+++ bonus_example_1.py
@@ -0,0 +1,2 @@
+book_title = input("Enter the title of your book: ")
+print("Book title is " + (str((len(book_title)))) + " characters long.")
---
+++ coding_exercise1.py
@@ -0,0 +1,8 @@
+name = "Michael.Wagner"
+print(type(name))
+
+email = input("Please enter your email address: ")
+print(email)
+
+cities = ["Chicago", "Itasca", "Elk Grove Village", "Schaumburg"]
+print(cities)
