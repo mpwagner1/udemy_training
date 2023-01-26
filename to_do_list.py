17afb7bfe032f45d0d81c0ab15cca8c2414a9b3d
Mike Wagner <mpwagner1@gmail.com>
2023-01-26 15:07:25 -0600

    committing branch Udemy day 2

--- to_do_list.py
+++ to_do_list.py
@@ -1,5 +1,7 @@
 # creating a to do list using python.  Will being making a terminal app, gui app and web app.
 user_input_prompt = "Enter a item for your to-do list: "
-
-user_text = input(user_input_prompt)
-print(user_text)
+to_do_list = []
+while True:
+    user_text = input(user_input_prompt)
+    to_do_list.append(user_text)
+print(todo_list)
