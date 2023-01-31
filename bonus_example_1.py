32dc40e7d34411df77118188fac948064e56938f
Mike Wagner <mpwagner1@gmail.com>
2023-01-31 11:10:02 -0600

    commiting new exercises from udemy course

--- bonus_example_1.py
+++ bonus_example_1.py
@@ -1,2 +1,15 @@
-book_title = input("Enter the title of your book: ")
-print("Book title is " + (str((len(book_title)))) + " characters long.")
+# Bonus example 1
+# book_title = input("Enter the title of your book: ")
+# print("Book title is " + (str((len(book_title)))) + " characters long.")
+
+# Bonus example 2
+# password = input("Enter password: ")
+# while password != "pass123":
+#     password = input("Enter password: ")
+# print("Password was correct!")
+
+# Bonus example 3
+# x = 1
+# while x <= 6:
+#     print(x)
+#     x = x + 1
--- coding_exercise1.py
+++ coding_exercise1.py
@@ -1,8 +1,20 @@
-name = "Michael.Wagner"
-print(type(name))
+# name = "Michael.Wagner"
+# print(type(name))
 
-email = input("Please enter your email address: ")
-print(email)
+# email = input("Please enter your email address: ")
+# print(email)
 
-cities = ["Chicago", "Itasca", "Elk Grove Village", "Schaumburg"]
-print(cities)
+# cities = ["Chicago", "Itasca", "Elk Grove Village", "Schaumburg"]
+# print(cities)
+
+
+# name = input(("Please enter your name: ")).capitalize()
+# print(name)
+
+# name = input(("Please enter your name: ")).capitalize()
+# while True:
+#     print(name)
+
+# while True:
+#     name = input("What is your name? "
+#     print (name.capitalize()))
